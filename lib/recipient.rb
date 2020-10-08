8d47a90b2b4ce4e641fb7822f42a8af7fb3b2a5f
Rachael Gomez <gomezrc1220@gmail.com>
2020-10-08 11:41:35 -0700

    created send_message method and tests for recipient_test.rb send message method is not passing its test

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,3 +1,4 @@
+POST_URL = "https://slack.com/api/chat.postMessage"
 class Recipient
   attr_reader :slack_id, :name
 
@@ -5,9 +6,15 @@ class Recipient
     @slack_id = slack_id
     @name = name
   end
-  # def send_meassage(message)
-  #
-  # end
+
+  def send_message(message)
+    params = {token: ENV['SLACK_TOKEN'], channel: @slack_id, text: message}
+    sleep(1)
+    puts params
+    # return self.class.error_message(HTTParty.post(POST_URL,params))
+    puts HTTParty.post(POST_URL,params)
+  end
+
   def self.get(url, params)
     sleep(1)
     return error_message(HTTParty.get(url,params))
--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -1,2 +1,58 @@
 require_relative 'test_helper'
 require_relative '../lib/recipient'
+
+describe "Recipient class" do
+
+  describe "instantiation" do
+
+    before do
+      @new_recipient = Recipient.new("some_id", "some_name")
+    end
+
+    it "is an instance of Recipient" do
+      expect(@new_recipient).must_be_instance_of Recipient
+    end
+
+    it "establishes the base data structures when instantiated" do
+      [:slack_id, :name].each do |keyword|
+        expect(@new_recipient).must_respond_to keyword
+      end
+
+      expect(@new_recipient.slack_id).must_be_kind_of String
+      expect(@new_recipient.name).must_be_kind_of String
+
+    end
+
+  end
+
+
+  describe "details" do
+    before do
+      @new_recipient = Recipient.new("some_id", "some_name")
+    end
+
+    it "raises an error" do
+      expect{@new_recipient.details}.must_raise NotImplementedError
+    end
+  end
+
+  describe "self.list_all" do
+    it "raises an error" do
+      expect{Recipient.list_all}.must_raise NotImplementedError
+    end
+  end
+
+  describe "send message" do
+    before do
+      @channel_recipient = Recipient.new("C01BKRLQ4UF", "random")
+      @user_recipient = Recipient.new("U01C0JB1FB4", "christabot")
+      @message = "Testing"
+    end
+    it "sends a message to a channel" do
+      VCR.use_cassette("post message to channel") do
+        @response = @channel_recipient.send_message(@message)
+      end
+      expect(@response).must_equal true
+    end
+  end
+end
