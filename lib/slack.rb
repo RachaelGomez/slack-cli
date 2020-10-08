dcd3e2f769e6d9cd6c561b92181bd1be3e133ea1
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-08 15:20:15 -0700

    Combined selections 3/4 in `main` case statement

--- lib/slack.rb
+++ lib/slack.rb
@@ -24,45 +24,31 @@ def main
       tp workspace.users, "slack_id", "name", "real_name", "status_text", "status_emoji"
     when 2
       tp workspace.channels, "slack_id", "name", "topic", "member_count"
-    when 3
-      puts "Please input a username or slack id"
-      selected_user = workspace.select_user(gets.chomp)
-      if selected_user
-        puts "Would you like details?"
+    when 3, 4
+      puts "Please input a name or slack id"
+      if selection == 3
+        selected_recipient = workspace.select_user(gets.chomp)
+      else
+        selected_recipient = workspace.select_channel(gets.chomp)
+      end
+
+      if selected_recipient
+        puts "Would you like details? (yes/no)"
         details_selection = input_validation(gets.chomp.downcase)
         if details_selection == "yes"
-          puts workspace.show_details(selected_user)
+          puts workspace.show_details(selected_recipient)
         end
-        puts "Would you like to send a message to #{selected_user.real_name}?"
+          puts "Would you like to #{selected_recipient.class == User ? "send a message" : "post"} to #{selected_recipient.name}? (yes/no)"
         message_selection = input_validation(gets.chomp.downcase)
         if message_selection == "yes"
           puts "Please write your message."
           message = gets.chomp
-          selected_user.send_message(message)
+          selected_recipient.send_message(message)
         end
       else
-        puts "No user found."
+        puts "No #{(selection == 3) ? "user" : "channel"} found."
       end
 
-    when 4
-      puts "Please input a channel name or slack id"
-      selected_channel = workspace.select_channel(gets.chomp)
-      if selected_channel
-        puts "Would you like details?"
-        details_selection = input_validation(gets.chomp.downcase)
-        if details_selection == "yes"
-          puts workspace.show_details(selected_channel)
-        end
-        puts "Would you like to make a post on #{selected_channel.name}?"
-        message_selection = input_validation(gets.chomp.downcase)
-        if message_selection == "yes"
-          puts "Please write your post."
-          message = gets.chomp
-          selected_channel.send_message(message)
-        end
-      else
-        puts "No channel found."
-      end
     end
 
     puts "Make another selection:"
@@ -76,7 +62,7 @@ end
 def number_validation(input, max_num)
   input = translate_input(input)
 
-  while !(1..max_num).include? input
+  until (1..max_num).include? input
     puts "Invalid input. Please re-enter a selection."
     input = translate_input(gets.chomp)
   end
